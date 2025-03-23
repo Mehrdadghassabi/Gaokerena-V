2aab59c4fd7388ebfa7eb41f92217142f2e6f2d4
mehrdadghassabi <53050138+Mehrdadghassabi@users.noreply.github.com>
2025-03-23 11:35:18 +0330

    Add files via upload

---
+++ dataset/MF3QA/crawling_and_filtering/dryab/main.py
@@ -0,0 +1,90 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+import os
+# install bs4,requests,pandas,openpyxl
+
+def getHTMLdocument(url):
+    response = requests.get(url)
+    return response.text
+
+def getQuestionlinks(url_to_scrape):
+    html_document = getHTMLdocument(url_to_scrape)
+    soup = BeautifulSoup(html_document, 'html.parser')
+    links = []
+    for link in soup.find_all('a',
+                              attrs={'href': re.compile("^/faq/")}):
+        links.append('https://doctor-yab.ir'+link.get('href'))
+    links = links[0:len(links) - 7]
+    return links
+
+def get_question(link):
+    req = requests.get(link)
+    html_content = req.content.decode('utf-8')
+    soup = BeautifulSoup(html_content, 'html.parser')
+    h3_tag = soup.find('div',itemprop="text", class_="col-xs-12 faq-text")
+    if h3_tag:
+        return h3_tag.getText()
+    else:
+        return "Tag not found."
+
+def get_answer(link):
+    req = requests.get(link)
+    html_content = req.content.decode('utf-8')
+    soup = BeautifulSoup(html_content, 'html.parser')
+    span = soup.find('p', class_="alert-warning2" , itemprop="text")
+    if span:
+        return span.getText()
+    else:
+        return ""
+
+def save_qas(url_to_scrape,file_name):
+    #url_to_scrape = "https://drhast.com/q/page-5"
+    links = getQuestionlinks(url_to_scrape)
+    data = {
+        'Question': [
+        ],
+        'Answer': [
+        ]
+    }
+    count = 0
+    for link in links:
+        data['Question'].append(get_question(link).strip())
+        data['Answer'].append(get_answer(link).strip())
+        count += 1
+
+    df = pd.DataFrame(data)
+    df.to_excel(file_name, index=False)
+    print(f"Data written to {file_name} successfully.")
+
+def merge_excel_files(folder_path):
+    all_data = []
+    for file in os.listdir(folder_path):
+        if file.endswith('.xlsx'):
+            file_path = os.path.join(folder_path, file)
+            df = pd.read_excel(file_path)
+            all_data.append(df)
+
+    merged_df = pd.concat(all_data, ignore_index=True)
+    merged_df = merged_df.drop_duplicates(subset=['Question'], keep='first')
+    merged_file_path = os.path.join(folder_path, 'merged_file.xlsx')
+    merged_df.to_excel(merged_file_path, index=False)
+
+def crawl_qas():
+    i = 1
+    for p in range(1, 4849):
+        try:
+            url_to_scrape = "https://doctor-yab.ir/faq/?page=" + str(p)
+            file_name = str(i) + '.xlsx'
+            save_qas(url_to_scrape, file_name)
+            i = i + 1
+        except Exception as e:
+            print(e)
+    folder_path = './'
+    merge_excel_files(folder_path)
+
+
+#crawl_qas()
+#merge_excel_files('./')
+#print(get_answer('https://doctor-yab.ir/faq/irq0/من-ناخواسته-باردار-شدم-و-قرصCYTOTECمصرف-مردم-یکم-خون-ریزی-داشتم-ولی-سقط-جنین-انجام-نشد-من-چیکار-کنم/').strip())
